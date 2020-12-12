1f92e057143d92796b9918e91bc53c10d21ac684
Pierce Lovesee <PierceLovesee@users.noreply.github.com>
2020-12-12 13:58:25 -0700

    added in assertions based on grader constraints

--- 1 - Algorithmic Toolbox/Week6_Dynamic_Programming2/1_maximum_amount_of_gold/knapsack.py
+++ 1 - Algorithmic Toolbox/Week6_Dynamic_Programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,18 @@
-# Uses python3
+#Pierce Lovesee
+#December 12th, 2020
 import sys
 
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+def optimal_weight(capacity, w):
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     W, n, *w = list(map(int, input.split()))
+
+    # assertions based on grader constraints
+    assert 1 <= W <= 10**4
+    assert 1 <= n <= 300
+    for i in w:
+        assert 1 <= i <= 10**5
+
     print(optimal_weight(W, w))
