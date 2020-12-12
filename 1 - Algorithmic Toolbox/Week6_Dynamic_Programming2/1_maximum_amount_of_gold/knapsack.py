5ce8fd54c83d2bd13b9c94905bafb4fc53d1416c
Pierce Lovesee <PierceLovesee@users.noreply.github.com>
2020-12-12 14:41:54 -0700

    working solution to knapsack problem; prior to changing loop order

--- 1 - Algorithmic Toolbox/Week6_Dynamic_Programming2/1_maximum_amount_of_gold/knapsack.py
+++ 1 - Algorithmic Toolbox/Week6_Dynamic_Programming2/1_maximum_amount_of_gold/knapsack.py
@@ -3,7 +3,35 @@
 import sys
 
 def optimal_weight(capacity, w):
+    """
+    Inputs
+    capacity: int, total capacity of the sack
+    w: list, weights of each bar of gold
 
+    Output
+    int, weight that maximizes gold taken, but does not exceed capacity
+    """
+    columns = len(w) + 1
+    rows = capacity + 1
+    # define 2 dimm list to hold possible results
+    # capacity in the y axis; available item weights in the x axis
+    Value = [[None] * columns for _ in range(rows)]
+
+    for row in range(rows): # populate first column with 0's
+        Value[row][0] = 0 # i.e. if no loot can be taken, value = 0
+
+    for column in range(1, columns): # available items
+        for row in range(rows):      # sack capacity
+            # start by skipping the item and then evaluating
+            Value[row][column] = Value[row][column - 1]
+
+            # row index value equivalent to
+            if row > w[column - 1]:
+                Value[row][column] = max(Value[row][column],
+                                    Value[row - w[column - 1]][column - 1] +
+                                     w[column - 1])
+                                     
+    return Value[capacity][len(w)]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
