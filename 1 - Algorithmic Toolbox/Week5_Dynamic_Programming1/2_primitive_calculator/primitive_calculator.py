d817da760a76f8bbef9075fd7a0be1f952eb95a2
Pierce Lovesee <PierceLovesee@users.noreply.github.com>
2020-12-11 18:08:54 -0700

    Implemented reconstruction algortithm to construct optimal path

--- 1 - Algorithmic Toolbox/Week5_Dynamic_Programming1/2_primitive_calculator/primitive_calculator.py
+++ 1 - Algorithmic Toolbox/Week5_Dynamic_Programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,6 +1,7 @@
 # Pierce Lovesee
 # December 11th, 2020
 # written in Python3; will not work in python2
+# I beleive this is due to an update to floating point arithmatic errors
 
 import sys
 
@@ -18,32 +19,26 @@ def optimal_sequence(n):
                 if numOps < minOps[i]:
                     minOps[i] = numOps
 
-    return minOps[n]
 
+    sequence = []
+    while n >= 1:
+        sequence.append(n)
+        operations = [n/3, n/2, n - 1]
+        previousOps = n**2
+        for operation in operations:
+            if (op % 1 == 0) and (minOps[int(operation)] == minOps[n] - 1):
+                proposedOps = minOps[int(operation)]
+                if proposedOps < previousOps:
+                    n = int(operation)
+    sequence.append(1)
 
-
-
-
-
-
-
-    #
-    # sequence = []
-    # while n >= 1:
-    #     sequence.append(n)
-    #     if n % 3 == 0:
-    #         n = n // 3
-    #     elif n % 2 == 0:
-    #         n = n // 2
-    #     else:
-    #         n = n - 1
-    # return reversed(sequence)
+    return reversed(sequence)
 
 input = sys.stdin.read()
 n = int(input)
 assert 1 <= n <= 10**6
 print(optimal_sequence(n))
-# sequence = list(optimal_sequence(n))
-# print(len(sequence) - 1)
-# for x in sequence:
-#     print(x, end=' ')
+sequence = list(optimal_sequence(n))
+print(len(sequence) - 1)
+for x in sequence:
+    print(x, end=' ')
