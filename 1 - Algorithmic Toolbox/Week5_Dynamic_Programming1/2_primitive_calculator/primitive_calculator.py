6a8eaeefc476a461336f28ccb23ed40dea45a617
Pierce Lovesee <PierceLovesee@users.noreply.github.com>
2020-12-11 17:09:16 -0700

    Attempt 1: trying to populate an array with the minimum operatoins

--- 1 - Algorithmic Toolbox/Week5_Dynamic_Programming1/2_primitive_calculator/primitive_calculator.py
+++ 1 - Algorithmic Toolbox/Week5_Dynamic_Programming1/2_primitive_calculator/primitive_calculator.py
@@ -2,20 +2,45 @@
 import sys
 
 def optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
-            n = n - 1
-    return reversed(sequence)
+    minOps = [0] * (n + 1)
+
+
+    for i in range(1, n + 1):
+        ops = [i/3, i/2, i - 1] # putting this here because i needs to be defined; may need to move for space
+        minOps[i] = n + 10
+
+        for op in ops:
+            if op % 1 == 0:
+                numOps = minOps[op] + 1
+                if numOps < minOps[i]:
+                    minOps[i] = numOps
+
+    return minOps[n]
+
+
+
+
+
+
+
+
+    #
+    # sequence = []
+    # while n >= 1:
+    #     sequence.append(n)
+    #     if n % 3 == 0:
+    #         n = n // 3
+    #     elif n % 2 == 0:
+    #         n = n // 2
+    #     else:
+    #         n = n - 1
+    # return reversed(sequence)
 
 input = sys.stdin.read()
 n = int(input)
-sequence = list(optimal_sequence(n))
-print(len(sequence) - 1)
-for x in sequence:
-    print(x, end=' ')
+assert 1 <= n <= 10**6
+print(optimal_sequence(n))
+# sequence = list(optimal_sequence(n))
+# print(len(sequence) - 1)
+# for x in sequence:
+#     print(x, end=' ')
