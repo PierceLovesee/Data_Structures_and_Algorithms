e0b486829efc6eaa55a0ce8192b537c5ceb3b777
Pierce Lovesee <PierceLovesee@users.noreply.github.com>
2020-12-13 17:41:06 -0700

    Added assertions to satisfy grader

--- 1 - Algorithmic Toolbox/Week6_Dynamic_Programming2/2_partitioning_souvenirs/partition3.py
+++ 1 - Algorithmic Toolbox/Week6_Dynamic_Programming2/2_partitioning_souvenirs/partition3.py
@@ -4,7 +4,7 @@ import itertools
 
 def partition3(A):
     # first determine if sum of elements in array is divisable by 3
-    if sum(a) % 3 != 0:
+    if sum(A) % 3 != 0:
         return 0
 
     p  = int(sum(a) / 3)
@@ -20,16 +20,33 @@ def partition3(A):
     # The rest of the 0th column initialized to false, other wise, this will
     # not work; everything in the table would evaluate to True
     # the 0th row has to be initalized to all True to represent that any valid
-    # combo of elements in A summing to p can have 0 added to it and 
+    # combo of elements in A summing to p can have 0 added to it and
     for i in range(1, p + 1):
         T[i][0] = False
 
-    for row in range()
+    for row in range(1, p + 1):
+        for column in range(1, n):
+            # initialize subject cell as skipping the column(i) element in A
+            T[row][column] = T[row][column - 1]
 
+            # check to see if A[column - 1] can be added in to the sum based on
+            # previous elements
+            if A[column - 1] >= row:
+                T[row][column] = ((T[row][column]) or
+                                    (T[row - A[column - 1]][column - 1]))
 
+    print(T[p])
 
+    if T[p][n -1]:
+        return 1
+    else:
+        return 0
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *A = list(map(int, input.split()))
+    assert len(A) == n
+    assert 1 <= n <= 20
+    for i in A:
+        assert 1 <= i <= 30
     print(partition3(A))
