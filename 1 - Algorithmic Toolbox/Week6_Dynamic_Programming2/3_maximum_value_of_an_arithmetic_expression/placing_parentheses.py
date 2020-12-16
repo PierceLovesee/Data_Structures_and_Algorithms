2b839735048180b865ffbf667b48e2fec3e99683
Pierce Lovesee <PierceLovesee@users.noreply.github.com>
2020-12-16 05:15:17 -0700

    begin work on problem 3; handel input data into useful lists

--- 1 - Algorithmic Toolbox/Week6_Dynamic_Programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ 1 - Algorithmic Toolbox/Week6_Dynamic_Programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -1,4 +1,6 @@
 # Uses python3
+import sys
+
 def evalt(a, b, op):
     if op == '+':
         return a + b
@@ -15,4 +17,12 @@ def get_maximum_value(dataset):
 
 
 if __name__ == "__main__":
-    print(get_maximum_value(input()))
+    data = list(map(str, sys.stdin.read().split()))
+    n = int((len(data) - 1) / 2) # n is the number of operators
+    assert 0 <= n <= 14  # assertion based on grader
+    values = [0] * (n + 1) # creats a list for holding 'int' values
+    for i in range(0, (n + 1)): # pulls out all numbers and puts in 'int' list
+        values[i] = int(data[i * 2])
+    ops = data[1:((2 * n) + 1):2] # pulls out all ops and puts in 'str' list
+
+    print(get_maximum_value(input())) # need to change "input()"
