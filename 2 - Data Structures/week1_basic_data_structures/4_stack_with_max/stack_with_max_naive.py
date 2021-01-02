c437d8258d65291c5ec18daf95adeb1c73deafe7
Pierce Lovesee <PierceLovesee@users.noreply.github.com>
2021-01-02 11:18:48 -0700

    Succesful Completion

--- 2 - Data Structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
+++ 2 - Data Structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
@@ -1,86 +1,44 @@
 #python3
 # Pierce Lovesee
 # Janurary 2nd, 2021
-# using implementation of random3 quick sort incase few eunique values
-# implementation developed in Algorithmic Tool box courser (course 1 of
-# this specializatoin)
 
 import sys
 from random import randint
 
-# def binary_search(keys, query):
-#     assert all(keys[i] < keys[i + 1] for i in range(len(keys) - 1))
-#     assert 1 <= len(keys) <= 3 * 10 ** 4
-#
-#     high = len(keys) - 1
-#     low = 0
-#
-#     while low <= high:
-#         mid = (low + ((high - low) // 2))
-#         if query == keys[mid]:
-#             return mid
-#         elif query < keys[mid]:
-#             high = mid - 1
-#         else:
-#             low = mid + 1
-#     return -1
-
-def partition3(array, left, right):
-    pivot = array[left]
-    lessThan = left
-    i = left
-    greaterThan = right
-
-    while i <= greaterThan:
-        if array[i] < pivot:
-            array[lessThan], array[i] = array[i], array[lessThan]
-            lessThan += 1
-            i += 1
-        elif array[i] > pivot:
-            array[i], array[greaterThan] = array[greaterThan], array[i]
-            greaterThan -= 1
-        else:
-            i += 1
-    return lessThan, greaterThan
-
-
-def randomized_quick_sort(array, left, right):
-    if left >= right:
-        return
-    k = randint(left, right)
-    array[left], array[k] = array[k], array[left]
-    lessThan, greaterThan = partition3(array, left, right)
-    randomized_quick_sort(array, left, lessThan - 1)
-    randomized_quick_sort(array, greaterThan + 1, right)
-
 
 class StackWithMax():
     def __init__(self):
-        self.__stack = []
-        self.MaxStack = []
+        self.__stack = []   # two stacks need to be maintained; one as stack
+        self.MaxStack = []  # and one to keep track of how many elemts can
+                            # be removed and have the current max stay the
+                            # max of the stack
 
-    def __str__(self):
-        return(str(self.__stack))
+    def __str__(self):              # print definintion for StackWithMax
+        return(str(self.__stack))   # object
 
-    def Push(self, a):
-        self.__stack.append(a)
+    def Push(self, a):  # handles new items being pushed to the stack
+        self.__stack.append(a)  # appends new item to end of stack
+        # if the MaxStack is empty, or if new element is larger than current
+        # max, then append to end of MaxStack list
         if (not bool(self.MaxStack)) or (self.MaxStack[-1] <= a):
             self.MaxStack.append(a)
+        # otherwise, the last element of the MaxStack is appened again
+        # conceptually this means that if there are say 5 copies of the
+        # same max element in a row, then 5 elements can be popped from
+        # the stack while maintaining the same max
         else:
-            self.MaxStack.append(a)
-            randomized_quick_sort(self.MaxStack, 0, len(self.MaxStack) - 1)
+            max = self.MaxStack[-1]
+            self.MaxStack.append(max)
 
 
-    def Pop(self):
+    def Pop(self):  # pops elements from both stacks
         assert(len(self.__stack))
-        if self.__stack[-1] == self.MaxStack[-1]:
-            self.__stack.pop()
-            self.MaxStack.pop()
-        else:
-            pop = self.__stack.pop()
-            self.MaxStack.remove(pop)
+        self.__stack.pop()
+        # as elements are popped from the main stack, the current maximum
+        # is tracked with the MaxStack stack.
+        self.MaxStack.pop()
 
-    def Max(self):
+    def Max(self):  # returns the last element of MaxStack
         return(self.MaxStack[-1])
 
 
