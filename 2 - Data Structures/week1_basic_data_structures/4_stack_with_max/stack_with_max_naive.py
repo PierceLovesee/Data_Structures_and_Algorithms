fc5c78c28abde3353f35e04186e55477b15f6bed
Pierce Lovesee <PierceLovesee@users.noreply.github.com>
2021-01-02 10:58:24 -0700

    changed to first check if the pushed value is greater than the max

--- 2 - Data Structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
+++ 2 - Data Structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
@@ -8,57 +8,80 @@
 import sys
 from random import randint
 
+# def binary_search(keys, query):
+#     assert all(keys[i] < keys[i + 1] for i in range(len(keys) - 1))
+#     assert 1 <= len(keys) <= 3 * 10 ** 4
+#
+#     high = len(keys) - 1
+#     low = 0
+#
+#     while low <= high:
+#         mid = (low + ((high - low) // 2))
+#         if query == keys[mid]:
+#             return mid
+#         elif query < keys[mid]:
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+#     return -1
+
+def partition3(array, left, right):
+    pivot = array[left]
+    lessThan = left
+    i = left
+    greaterThan = right
+
+    while i <= greaterThan:
+        if array[i] < pivot:
+            array[lessThan], array[i] = array[i], array[lessThan]
+            lessThan += 1
+            i += 1
+        elif array[i] > pivot:
+            array[i], array[greaterThan] = array[greaterThan], array[i]
+            greaterThan -= 1
+        else:
+            i += 1
+    return lessThan, greaterThan
+
+
+def randomized_quick_sort(array, left, right):
+    if left >= right:
+        return
+    k = randint(left, right)
+    array[left], array[k] = array[k], array[left]
+    lessThan, greaterThan = partition3(array, left, right)
+    randomized_quick_sort(array, left, lessThan - 1)
+    randomized_quick_sort(array, greaterThan + 1, right)
+
+
 class StackWithMax():
     def __init__(self):
         self.__stack = []
+        self.MaxStack = []
 
     def __str__(self):
         return(str(self.__stack))
 
     def Push(self, a):
         self.__stack.append(a)
+        if (not bool(self.MaxStack)) or (self.MaxStack[-1] <= a):
+            self.MaxStack.append(a)
+        else:
+            self.MaxStack.append(a)
+            randomized_quick_sort(self.MaxStack, 0, len(self.MaxStack) - 1)
+
 
     def Pop(self):
         assert(len(self.__stack))
-        self.__stack.pop()
+        if self.__stack[-1] == self.MaxStack[-1]:
+            self.__stack.pop()
+            self.MaxStack.pop()
+        else:
+            pop = self.__stack.pop()
+            self.MaxStack.remove(pop)
 
     def Max(self):
-        assert(len(self.__stack))
-        MaxStack = []
-        for i in self.__stack:
-            MaxStack.append(i)
-
-
-        def partition3(array, left, right):
-            pivot = array[left]
-            lessThan = left
-            i = left
-            greaterThan = right
-
-            while i <= greaterThan:
-                if array[i] < pivot:
-                    array[lessThan], array[i] = array[i], array[lessThan]
-                    lessThan += 1
-                    i += 1
-                elif array[i] > pivot:
-                    array[i], array[greaterThan] = array[greaterThan], array[i]
-                    greaterThan -= 1
-                else:
-                    i += 1
-            return lessThan, greaterThan
-
-
-        def randomized_quick_sort(array, left, right):
-            if left >= right:
-                return
-            k = randint(left, right)
-            array[left], array[k] = array[k], array[left]
-            lessThan, greaterThan = partition3(array, left, right)
-            randomized_quick_sort(array, left, lessThan - 1)
-            randomized_quick_sort(array, greaterThan + 1, right)
-
-        randomized_quick_sort(MaxStack, 0, len(MaxStack) - 1)
-        return(MaxStack[-1])
+        return(self.MaxStack[-1])
 
 
 if __name__ == '__main__':
