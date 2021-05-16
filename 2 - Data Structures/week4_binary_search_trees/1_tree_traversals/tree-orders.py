bd4723699fd38a10bde869cc57f9e694cd2129a7
PierceLovesee <PierceLovesee@users.noreply.github.com>
2021-05-16 01:04:36 -0500

    Working recursive inOrder procedure.

--- 2 - Data Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
+++ 2 - Data Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
@@ -1,8 +1,8 @@
 # python3
 
 import sys, threading
-sys.setrecursionlimit(10**8) # max depth of recursion
-threading.stack_size(2**30)  # new thread will get stack of such size
+sys.setrecursionlimit(10**6) # max depth of recursion
+threading.stack_size(2**27)  # new thread will get stack of such size
 
 class TreeOrders:
     def read(self):
@@ -20,31 +20,31 @@ class TreeOrders:
             self.left[i] = b
             self.right[i] = c
 
-    def inOrder(self, i): # In-Order: Left --> Node --> Right
+    def inOrder(self, n): # In-Order: Left --> Node --> Right
     ## need to totally reevalute how you are attacking this problem.
-    ## -1 does not exist int he key list, -1 is found in the child list (left & right) 
+    ## -1 does not exist int he key list, -1 is found in the child list (left & right)
 
-        if self.key[i] == -1:
+        if self.left[n] == -1:
+            print(self.key[n], end = ' ')
             return
-        self.inOrder(self.left[i])
-        print(self.key[i], end = ' ')
-        self.inOrder(self.right[i])
+        self.inOrder(self.left[n])
+        print(self.key[n], end = ' ')
+        self.inOrder(self.right[n])
 
-
-    def preOrder(self): # Pre-Order: Node --> Left --> Right
-        if self.key == -1:
-            return
-        print(self.key, end = ' ')
-        self.inOrder(self.left)
-        self.inOrder(self.right)
-
-
-    def postOrder(self): # Post-Order: Left --> Right --> Node
-        if self.key == -1:
-            return
-        self.postOrder(self.left)
-        self.postOrder(self.right)
-        print(self.key, end = ' ')
+    # def preOrder(self): # Pre-Order: Node --> Left --> Right
+    #     if self.key == -1:
+    #         return
+    #     print(self.key, end = ' ')
+    #     self.inOrder(self.left)
+    #     self.inOrder(self.right)
+    #
+    #
+    # def postOrder(self): # Post-Order: Left --> Right --> Node
+    #     if self.key == -1:
+    #         return
+    #     self.postOrder(self.left)
+    #     self.postOrder(self.right)
+    #     print(self.key, end = ' ')
 
 
 def main():
