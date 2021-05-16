282e6aa1755d1606d9a6ebc36d53b8d5967005c7
PierceLovesee <PierceLovesee@users.noreply.github.com>
2021-05-16 02:56:17 -0500

    Final passing revision; recursive solution to tree traversal.

--- 2 - Data Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
+++ 2 - Data Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
@@ -1,4 +1,7 @@
 # python3
+# Pierce Lovesee
+# May 15th, 2021
+# Max time used: 0.78/6.00, max memory used: 116871168/536870912.
 
 import sys, threading
 sys.setrecursionlimit(10**6) # max depth of recursion
@@ -6,9 +9,11 @@ threading.stack_size(2**27)  # new thread will get stack of such size
 
 class TreeOrders:
     def read(self):
-        #all indexes of children and keys initalized to 0
-        #during input, if a node does not have a child, the corrisoponding indexes
-        #are set to -1 for logical testing
+        #memory alocated for structure by initalizing arrays of size n to zero,
+        #3 arrays are created, one for the values at each node,  then two to
+        #hold the index of each nodes left and right child.
+        #if a node does not have a child, the corrisoponding indexes
+        # are set to -1 for logical testing
         self.n = int(sys.stdin.readline())
         self.key = [0 for i in range(self.n)]
         self.left = [0 for i in range(self.n)]
@@ -21,37 +26,38 @@ class TreeOrders:
             self.right[i] = c
 
     def inOrder(self, n): # In-Order: Left --> Node --> Right
-    ## need to totally reevalute how you are attacking this problem.
-    ## -1 does not exist int he key list, -1 is found in the child list (left & right)
+        # base case: once n is -1 we know we have gone as far left as possible
+        if n == -1:
+            return # so end recursion and return to previous stack frame
+        self.inOrder(self.left[n]) # call on left child until base case reached
+        print(self.key[n], end = ' ') # visit node in 'previous' frame
+        self.inOrder(self.right[n]) # call on right child
 
-        if self.left[n] == -1:
-            print(self.key[n], end = ' ')
-            return
-        self.inOrder(self.left[n])
-        print(self.key[n], end = ' ')
-        self.inOrder(self.right[n])
+    def preOrder(self, n): # Pre-Order: Node --> Left --> Right
+        # base case: once n is -1 we know we have gone as far left as possible
+        if n == -1:
+            return # so end recursion and return to previous stack frame
+        print(self.key[n], end = ' ') # visit node along route going left
+        self.preOrder(self.left[n]) # call on left child
+        self.preOrder(self.right[n]) # call on right children
 
-    # def preOrder(self): # Pre-Order: Node --> Left --> Right
-    #     if self.key == -1:
-    #         return
-    #     print(self.key, end = ' ')
-    #     self.inOrder(self.left)
-    #     self.inOrder(self.right)
-    #
-    #
-    # def postOrder(self): # Post-Order: Left --> Right --> Node
-    #     if self.key == -1:
-    #         return
-    #     self.postOrder(self.left)
-    #     self.postOrder(self.right)
-    #     print(self.key, end = ' ')
+
+    def postOrder(self, n): # Post-Order: Left --> Right --> Node
+        # base case: once n is -1 we know we have gone as far left as possible
+        if n == -1:
+            return # so end recursion and return to previous stack frame
+        self.postOrder(self.left[n]) #first recursivly call on left sub-tree
+        self.postOrder(self.right[n])#second recursivly call on right sub-tree
+        print(self.key[n], end = ' ')#visit node after both children visited
 
 
 def main():
-    tree = TreeOrders()
-    tree.read()
-    tree.inOrder(0)
-    # tree.preOrder()
-    # tree.postOrder()
+    tree = TreeOrders() # creat an instance of a TreeOrders object
+    tree.read() # read in the values and structure of the tree from user input
+    tree.inOrder(0) # traverse the tree inorder begining with the root node
+    print("") #create new line in output print
+    tree.preOrder(0) # traverse the tree in preorder starting with the root node
+    print("") # create new line in output print
+    tree.postOrder(0) # traverse the tree in postorder starting with root node
 
 threading.Thread(target=main).start()
