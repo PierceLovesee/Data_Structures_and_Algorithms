614a03f5ad44303a6c36e9e6a7ba1a86c6f34f10
PierceLovesee <PierceLovesee@users.noreply.github.com>
2021-07-02 22:33:25 -0500

    Completed assignment

--- 3 - Algorithms on Graphs/week2_graph_decomposition2/2_order_of_courses/toposort.py
+++ 3 - Algorithms on Graphs/week2_graph_decomposition2/2_order_of_courses/toposort.py
@@ -1,17 +1,29 @@
 #Uses python3
+#Pierce Lovesee
+#July 2nd, 2021
+#Good job! (Max time used: 0.27/10.00, max memory used: 39157760/536870912.)
 
 import sys
 
-def dfs(adj, used, order, x):
-    #write your code here
-    pass
+def toposort(adj, visitedNodes):
+
+    postOrdering = []  #keep track of the post ordering of the graph
+
+    def explore(v):
+        if visitedNodes[v]: #if the vertex has been visted, don't explore it
+            return
+        for i in adj[v]: #explore all adjacent verticies to 'v'
+            explore(i)
+        visitedNodes[v] = True #mark as visited when adjacent verticies explored
+        postOrdering.append(v) #append vertex to post ordering record
 
 
-def toposort(adj):
-    used = [0] * len(adj)
-    order = []
-    #write your code here
-    return order
+    def DFS(): #perform DFS on graph to determine post ordering
+        for v in range(len(visitedNodes)): #explore all verticies in the graph
+            explore(v)
+        return(postOrdering) #once complete, return te post ordering of graph
+
+    return(DFS())
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -22,7 +34,7 @@ if __name__ == '__main__':
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
-    order = toposort(adj)
-    for x in order:
-        print(x + 1, end=' ')
-
+    visitedNodes = [False for _ in range(n)]
+    order = toposort(adj, visitedNodes)
+    for _ in range(n):
+        print(order.pop() + 1, end=' ')
