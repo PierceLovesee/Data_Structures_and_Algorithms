4e291d8bf5cf8f65ec7cd0c6ce0d01746f58eed0
PierceLovesee <PierceLovesee@users.noreply.github.com>
2021-07-04 09:10:45 -0500

    Added comments

--- 3 - Algorithms on Graphs/week3_paths_in_graphs1/2_bipartite/bipartite.py
+++ 3 - Algorithms on Graphs/week3_paths_in_graphs1/2_bipartite/bipartite.py
@@ -1,26 +1,32 @@
 #Uses python3
+#Pierce Lovesee
+#July 4th, 2020
 
 import sys
 import queue
 
 def bipartite(adj):
-    dist = [-1 for _ in range(len(adj))]
-
+    dist = [-1 for _ in range(len(adj))] #distance of each node from origin
+    # Function to execute the breadth-first search
     def BFS():
-        dist[0] = 0
-        Q = [0]
-        while bool(Q):
-            u = Q.pop(0)
-            for i in adj[u]:
-                if dist[i] == -1:
-                    Q.append(i)
-                    dist[i] = dist[u] + 1
-                else:
+        dist[0] = 0 #set the distance at the first node to 0
+        Q = [0] # add the first node to the queue
+        while bool(Q): #do the following while the queue is not empty
+            u = Q.pop(0) #dequeue the next node in the queue, 'u'
+            for i in adj[u]: #for each adjacent node to 'u'
+                if dist[i] == -1: #if it has not been visited
+                    Q.append(i) #add it to the queue
+                    dist[i] = dist[u] + 1 #add set it's distance to dist[u] + 1
+                else: #otherwise, if the node has been visited
+                    #check if the two nodes belong to different Sets
+                    #this is simply checking that only even level nodes are
+                    #connected to odd level nodes, and visa-versa.
                     if ((dist[u] % 2) == (dist[i] % 2)):
-                        return 0
-        return 1
-        
-    return(BFS())
+                        return 0 #if even is connected to even, or odd to odd,
+                        #then return 0 signifying that the graph is not bipartite
+        return 1 #If the BFS executes succesfully, the graph is bipartite
+
+    return(BFS()) #return the result fo the BFS
 
 if __name__ == '__main__':
     input = sys.stdin.read()
