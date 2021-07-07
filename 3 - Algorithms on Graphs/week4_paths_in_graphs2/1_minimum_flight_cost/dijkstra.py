be8abd780dd71b99451e7eed3d68925aba402efe
PierceLovesee <PierceLovesee@users.noreply.github.com>
2021-07-07 01:30:34 -0500

    added comments

--- 3 - Algorithms on Graphs/week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.py
+++ 3 - Algorithms on Graphs/week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.py
@@ -8,25 +8,27 @@ import queue
 
 def distance(adj, cost, s, t, dist, prev):
 
-    def dijkstra(s):
-        Q = [[float('inf'), i] for i in range(len(adj))]
-        Q[s][0] = 0
-        dist[0] = 0
-        while bool(Q):
-            Q = sorted(Q)
-            u = Q.pop(0)
-            if u[0] == float('inf'):
+    def dijkstra(s): #implemendation of Dijkstra's algorithm
+        Q = [[float('inf'), i] for i in range(len(adj))] #min-priority queue
+        # Q is prioritized based on minimum distance from 's'
+        Q[s][0] = 0 # set distance to 's' to 0 in queue
+        dist[0] = 0 # set distance in dist array to 0 for 's'
+        while bool(Q): #while the queue is not empty, do the following:
+            Q = sorted(Q) #sort Q by distace and save as the new array
+            u = Q.pop(0) #dequeue the node with the shortest distance from 's'
+            if u[0] == float('inf'): #if 'u's distance from 's' is inf., break
                 break
+            # otherwise, do the following for all of 'u's adjacent nodes:
             for n, v in enumerate(adj[u[1]]):
-                if dist[v] > dist[u[1]] + cost[u[1]][n]:
-                    dist[v] = dist[u[1]] + cost[u[1]][n]
-                    Q.append([dist[v], v])
-                    prev[v] = u[1]
-    dijkstra(s)
-    tDist = dist[t]
-    if tDist == float('inf'):
-        return -1
-    return tDist
+                if dist[v] > dist[u[1]] + cost[u[1]][n]: #if a shorter path is found
+                    dist[v] = dist[u[1]] + cost[u[1]][n] #save that path
+                    Q.append([dist[v], v]) #and add that node to the queue
+                    prev[v] = u[1]# record the previous node for the shortest path
+    dijkstra(s) #envoke Dijkstra's algorithm
+    tDist = dist[t] #determine the cost for 's' to 't'
+    if tDist == float('inf'): #if 't' is unreachable from 's'
+        return -1 #then return error
+    return tDist #otherwise, return the cost-distance from 's' to 't'
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -43,6 +45,6 @@ if __name__ == '__main__':
         adj[a - 1].append(b - 1)
         cost[a - 1].append(w)
     s, t = data[0] - 1, data[1] - 1 #s - source node | t - target node
-    dist = [float('inf') for _ in range(n)]
-    prev = [-1 for _ in range(n)]
+    dist = [float('inf') for _ in range(n)] #array to store distance from source
+    prev = [-1 for _ in range(n)] #array for tracking previous node
     print(distance(adj, cost, s, t, dist, prev))
