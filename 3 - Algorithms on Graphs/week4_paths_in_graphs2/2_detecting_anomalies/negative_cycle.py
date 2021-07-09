0f301db5505a2cd49b68c5248e0db98c9fbfa5d2
PierceLovesee <PierceLovesee@users.noreply.github.com>
2021-07-08 20:44:59 -0500

    Needs debugging, not detecting negative cycle.  Identify source and sink

--- 3 - Algorithms on Graphs/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
+++ 3 - Algorithms on Graphs/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
@@ -1,16 +1,43 @@
 #Uses python3
+#Pierce Lovesee
+#July 8th, 2021
 
 import sys
 
+def negCycleDetect(adj, cost):
+    dist = [float('inf') for _ in range(len(adj))]
+    prev = [-1 for _ in range(len(adj))]
+    dist[0] = 0
 
-def negative_cycle(adj, cost):
-    #write your code here
-    return 0
+    def relax(u, v, v_n):
+        if dist[v] > dist[u] + cost[u][v_n]:
+            dist[v] = dist[u] + cost[u][v_n]
+            prev[v] = u
+
+    def detectNegativeCycle():
+        distCycle1 = dist
+        for u in range(len(adj)):
+            for n, v in enumerate(adj[u]):
+                relax(u, v, n)
+                if dist[v] < distCycle1[v]:
+                    return 1
+        return 0
+
+    def Bellman_Ford():
+        for u in range(len(adj)):
+            for n, v in enumerate(adj[u]):
+                relax(u, v, n)
+        return(detectNegativeCycle())
+
+
+    return(Bellman_Ford())
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    # input = sys.stdin.read()
+    # data = list(map(int, input.split()))
+    # data = [4, 4, 1, 2, 1, 4, 1, 2, 2, 3, 2, 1, 3, 5] # 0
+    data = [4, 4, 1, 2, -5, 4, 1, 2, 2, 3, 2, 3, 1, 1]
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
@@ -20,4 +47,4 @@ if __name__ == '__main__':
     for ((a, b), w) in edges:
         adj[a - 1].append(b - 1)
         cost[a - 1].append(w)
-    print(negative_cycle(adj, cost))
+    print(negCycleDetect(adj, cost))
