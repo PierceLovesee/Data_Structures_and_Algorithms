5e9b16b84831aeb331254ce9aad4a4fc8b6a4ce6
PierceLovesee <PierceLovesee@users.noreply.github.com>
2021-08-06 14:22:33 -0600

    Updated

--- 3 - Algorithms on Graphs/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
+++ 3 - Algorithms on Graphs/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
@@ -7,39 +7,68 @@ import sys
 def negCycleDetect(adj, cost):
     dist = [float('inf') for _ in range(len(adj))]
     prev = [-1 for _ in range(len(adj))]
-    dist[0] = 0
+    negCycleFound = 0
 
     def relax(u, v, v_n):
         if dist[v] > dist[u] + cost[u][v_n]:
             dist[v] = dist[u] + cost[u][v_n]
             prev[v] = u
 
-    def detectNegativeCycle():
+    def detectNegativeCycle(start, passedQ):
+        nonlocal negCycleFound
         distCycle1 = []
         for i in range(len(adj)):
             distCycle1.append(dist[i])
-        for u in range(len(adj)):
+        while bool(passedQ):
+            if negCycleFound == 1:
+                break
+            u = passedQ.pop(0)
             for n, v in enumerate(adj[u]):
+                passedQ.append(v)
                 relax(u, v, n)
                 if dist[v] < distCycle1[v]:
-                    return 1
-        return 0
+                    negCycleFound = 1
 
-    def Bellman_Ford():
-        for u in range(len(adj)):
+    # this may be breaking the loop before all nodes are relaxed.
+    # then it thinks a
+    # may need to run DFS to find all reachable nodes first
+
+    def Bellman_Ford(u):
+        start = u
+        Q = []
+        Q.append(u)
+        passedQ = []
+        passedQ.append(u)
+        while bool(Q):
+            u = Q.pop(0)
+            if negCycleFound == 1:
+                break
             for n, v in enumerate(adj[u]):
+                if v in passedQ:
+                    break
+                Q.append(v)
+                passedQ.append(v)
                 relax(u, v, n)
-        return(detectNegativeCycle())
+        detectNegativeCycle(start, passedQ)
 
+    def disJointGraphs():
+        for u in range(len(adj)):
+            if negCycleFound == 1:
+                break
+            if dist[u] == float('inf'):
+                dist[u] = 0
+                Bellman_Ford(u)
+        return negCycleFound
 
-    return(Bellman_Ford())
+    return(disJointGraphs())
 
 
 if __name__ == '__main__':
     # input = sys.stdin.read()
     # data = list(map(int, input.split()))
-    # data = [4, 4, 1, 2, 1, 4, 1, 2, 2, 3, 2, 1, 3, 5] # 0
-    data = [4, 4, 1, 2, -5, 4, 1, 2, 2, 3, 2, 3, 1, 1]
+    data = [4, 4, 1, 2, 1, 4, 1, 2, 2, 3, 2, 1, 3, 5] # 0
+    # data = [4, 4, 1, 2, -5, 4, 1, 2, 2, 3, 2, 3, 1, 1] # 1
+    # data = [10, 9, 1, 2, 1, 5, 6, 1, 6,  7, 1, 8, 9, 1, 9, 10, 1, 3, 4, 1, 7, 8, 1, 4, 5, 1, 2, 3, 1] # 0
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
