546e6d95ebeb940995f0ce2965ec2d3809a279b2
Pierce <pierce.lovesee@gmail.com>
2021-08-15 00:25:46 -0600

    Working solution

--- 3 - Algorithms on Graphs/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
+++ 3 - Algorithms on Graphs/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
@@ -1,45 +1,48 @@
 #Uses python3
 #Pierce Lovesee
 #July 8th, 2021
+#Good job! (Max time used: 2.76/10.00, max memory used: 12136448/536870912.)
 
 import sys
 
 def negCycleDetect(adj, cost):
-    dist = [float('inf') for _ in range(len(adj))]
-    prev = [-1 for _ in range(len(adj))]
-    dist[0] = 0
+    dist = [1001 for _ in range(len(adj))] #set dist to 'large' value
+    prev = [-1 for _ in range(len(adj))] #initial previous node to -1
+    dist[0] = 0 #start with node #0, dist 0
 
-    def relax(u, v, v_n):
-        if dist[v] > dist[u] + cost[u][v_n]:
-            dist[v] = dist[u] + cost[u][v_n]
-            prev[v] = u
+    def relax(u, v, v_n): #relax function for optimizing path
+        if dist[v] > dist[u] + cost[u][v_n]: #if a shorter path is found
+            dist[v] = dist[u] + cost[u][v_n] #update the path
+            prev[v] = u # and note the previous node
 
-    def detectNegativeCycle():
-        distCycle1 = []
-        for i in range(len(adj)):
-            distCycle1.append(dist[i])
-        for u in range(len(adj)):
-            for n, v in enumerate(adj[u]):
-                relax(u, v, n)
-                if dist[v] < distCycle1[v]:
-                    return 1
-        return 0
+    def detectNegativeCycle(): #determine if a negative cycle exists
+        distCycle1 = [] # blank list copying over dist list
+        for i in range(len(adj)): #look at each element in the dist list
+            distCycle1.append(dist[i]) # and then copy it to the new list
+        #this time, only do the following once
+        for u in range(len(adj)): #for each node
+            for n, v in enumerate(adj[u]): #look at each node in adj list
+                relax(u, v, n) #and relax it
+                if dist[v] < distCycle1[v]: #if a shorter path is found at any point
+                    return 1 #then a negative cycle has been found
+        return 0 # otherwise, no negative cycle exists
 
-    def Bellman_Ford():
-        for u in range(len(adj)):
-            for n, v in enumerate(adj[u]):
-                relax(u, v, n)
-        return(detectNegativeCycle())
+    def Bellman_Ford(): #implementation of Bellman-Ford Algorithm
+        for _ in range(len(adj)-1): # do the follwing N-1 times
+            for u in range(len(adj)): #look at every node
+                for n, v in enumerate(adj[u]): #and for all nodes in adj list
+                    relax(u, v, n) #relax that node
+        return(detectNegativeCycle()) #Then, see if neg. cycle exits
 
 
-    return(Bellman_Ford())
+    return(Bellman_Ford()) #envoke Bellman-Ford Algorithm
 
 
 if __name__ == '__main__':
-    # input = sys.stdin.read()
-    # data = list(map(int, input.split()))
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
     # data = [4, 4, 1, 2, 1, 4, 1, 2, 2, 3, 2, 1, 3, 5] # 0
-    data = [4, 4, 1, 2, -5, 4, 1, 2, 2, 3, 2, 3, 1, 1]
+    # data = [4, 4, 1, 2, -5, 4, 1, 2, 2, 3, 2, 3, 1, 1] # 1
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
